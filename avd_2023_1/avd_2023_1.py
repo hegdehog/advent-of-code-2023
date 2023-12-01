e2ea1762778b673ac570a6fdc6e54541ca700b7b
Marcos Torregrosa <nagash87@gmail.com>
2023-12-01 13:38:55 +0100

    Update avd_2023_1.py

--- avd_2023_1/avd_2023_1.py
+++ avd_2023_1/avd_2023_1.py
@@ -1,7 +1,7 @@
 dictPuzzle2 = {"one" : "1", "two" : "2",  "three" : "3", "four" : "4", "five" : "5", "six" : "6", "seven" : "7", "eight" : "8",  "nine" : "9"
                ,"1":"1","2":"2","3":"3","4":"4","5":"5","6":"6","7":"7","8":"8","9":"9"}
 
-path = "C:\\Users\\GU60TR\\OneDrive - ING\\Documentos\\2. Data Strategy\\5.Advent of code\\1\\stars.txt"
+path = "stars.txt"
 
 def calcualteTotal( numbers ):
     return  int(numbers[0]) *10 + int(numbers[-1])
